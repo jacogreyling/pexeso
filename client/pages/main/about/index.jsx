d08b7450a5911e1109bf284cab90754fd24ef7eb
CA-CharlKlein <Charl.Klein@CaAfrica.co.za>
2017-09-05 15:05:39 +0200

    Correct Fix for Github Log
    Logo Included, Alt Test Added Closed #24

--- client/pages/main/about/index.jsx
+++ client/pages/main/about/index.jsx
@@ -78,7 +78,7 @@ class AboutPage extends React.Component {
                             </a>&nbsp;project on Github and help us build Pexeso!
                         </p>
                         <a href="https://github.com/jacogreyling/pexeso" target="_blank">
-                            <img height="300" width="300" src="https://s3.eu-west-2.amazonaws.com/pexeso.io/public/Github.png" 
+                            <img height="300" width="300" src="public/media/github.png" 
                                  alt="Fork this project on Github and help us build pexeso" />
                         </a>
                     </div>
