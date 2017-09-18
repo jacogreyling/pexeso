aeea5d8a28f646f4a30fdc9bbaeb470247591185
CA-CharlKlein <Charl.Klein@CaAfrica.co.za>
2017-09-18 10:09:57 +0200

    Updated Shuffle Logic
    Closed #39

--- client/helpers/shuffle.js
+++ client/helpers/shuffle.js
@@ -1,14 +1,40 @@
 module.exports = function(array) {
-    let currentIndex = array.length, temp, randomIndex ;
+    let arrayLength = array.length;
+    let shuffleArray =[];
+    let reshuffleArray =[];
+    let sourceArray = array;
 
-    while (0 !== currentIndex) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
+    while (arrayLength !== 0 ) {
 
-        temp = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temp;
+        //reduce the remaining elements by 1
+        let remainingElements = arrayLength--;
+
+        // Pick a remaining element…
+        let randomElement = Math.floor(Math.random() * remainingElements);
+
+        //move random element in to shuffle array
+        shuffleArray.push(sourceArray.splice(randomElement, 1)[0]);
+
+        // Find tiles next to each other
+        if (shuffleArray.length > 2){
+            let length = shuffleArray.length;
+            if (shuffleArray[(length - 2)].id == shuffleArray[(length - 1)].rel){
+                reshuffleArray.push(length-1);
+            }
+        }
     }
+   
+    //move tiles that were next to each other
+    reshuffleArray.forEach(function(element) {
+        //Tile to Move
+        let elementToMove = shuffleArray.splice(element,1)[0];
 
-    return array;
+        //New Position 
+        let newPosition = Math.floor(Math.random() * (shuffleArray.length - reshuffleArray.length));
+
+        //Insert Element @ New Position
+        shuffleArray.splice(newPosition, 0, elementToMove);
+    });
+
+    return shuffleArray;
 }
